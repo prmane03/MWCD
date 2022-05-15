dfc72f3ce6988f0f4ce77a487afaab34ee3e3dcb
prmane03 <prmane03@gmail.com>
2022-05-15 20:51:29 +0530

    All Components in main module created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './main/about/about.component';
+import { AdminComponent } from './main/admin/admin.component';
+import { FaqComponent } from './main/faq/faq.component';
+import { HomeComponent } from './main/home/home.component';
+import { HostelComponent } from './main/hostel/hostel.component';
+import { LegislationComponent } from './main/legislation/legislation.component';
+import { NgoComponent } from './main/ngo/ngo.component';
+import { StepComponent } from './main/step/step.component';
+import { SukanyaComponent } from './main/sukanya/sukanya.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'homePage', component: HomeComponent },
+  { path: 'aboutPage', component: AboutComponent },
+  { path: 'legislationPage', component: LegislationComponent },
+  { path: 'stepPage', component: StepComponent },
+  { path: 'ngoPage', component: NgoComponent },
+  { path: 'adminPage', component: AdminComponent },
+  { path: 'sukanyaPage', component: SukanyaComponent },
+  { path: 'faqPage', component: FaqComponent },
+  { path: 'hostelPage', component: HostelComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './main/home/home.component';
+import { AboutComponent } from './main/about/about.component';
+import { LegislationComponent } from './main/legislation/legislation.component';
+import { StepComponent } from './main/step/step.component';
+import { NgoComponent } from './main/ngo/ngo.component';
+import { AdminComponent } from './main/admin/admin.component';
+import { HostelComponent } from './main/hostel/hostel.component';
+import { SukanyaComponent } from './main/sukanya/sukanya.component';
+import { FaqComponent } from './main/faq/faq.component';
+import { LoginComponent } from './main/login/login.component';
+import { RegisterComponent } from './main/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    LegislationComponent,
+    StepComponent,
+    NgoComponent,
+    AdminComponent,
+    HostelComponent,
+    SukanyaComponent,
+    FaqComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
