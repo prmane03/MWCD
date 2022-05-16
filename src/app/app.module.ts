c25cb2e1c3027ca6dc3a95c1a926f86e974cd7ab
Richa2799 <rwakodikar99@gmail.com>
2022-05-16 10:45:32 +0530

    ngo changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
