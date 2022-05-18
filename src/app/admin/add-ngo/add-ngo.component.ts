9b886b7e724bea9473c3513deb1e305280641b50
Paras Bora <parasborarm7@gmail.com>
2022-05-18 11:19:19 +0530

    mwcd updated

--- src/app/admin/add-ngo/add-ngo.component.ts
+++ src/app/admin/add-ngo/add-ngo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-ngo',
@@ -10,6 +11,17 @@ export class AddNgoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  // form validation
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    dir: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
+
+  get f(){
+    return this.form.controls;
   }
 
 }
--- src/app/hostel/hostel-location/hostel-location.component.ts
+++ src/app/hostel/hostel-location/hostel-location.component.ts
@@ -11,6 +11,10 @@ export class HostelLocationComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
+
+
+  // getLocation():any
+
+
 
 }
