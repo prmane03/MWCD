e6bd552dea984024eecd03a1c2b4ecb54009fa64
prmane03 <prmane03@gmail.com>
2022-05-18 21:56:33 +0530

    changes done in registration

--- src/app/ngo/ngoregister/ngoregister.component.ts
+++ src/app/ngo/ngoregister/ngoregister.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NGOService } from 'src/app/Services/ngo.service';
 
 @Component({
@@ -10,21 +10,23 @@ import { NGOService } from 'src/app/Services/ngo.service';
 })
 export class NgoregisterComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, public ng: NGOService) { }
-
+  constructor(private route: ActivatedRoute, public ns: NGOService,private router: Router) { }
+  ngoid:any ;
   role:string;
+  // stepStatus:any;
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.role=params['type'];
+      this.ngoid=localStorage.getItem('id');
+    //  this.stepStatus=localStorage.getItem('stepStatus');
     });
   }
   
 
   // form validation
   form = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    // name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    // email: new FormControl('', [Validators.required, Validators.email]),
     director: new FormControl('', [Validators.required, Validators.minLength(3)]),
     addressLine: new FormControl('', [Validators.required]),
     city: new FormControl('', [Validators.required]),
@@ -39,53 +41,24 @@ export class NgoregisterComponent implements OnInit {
     return this.form.controls;
   }
 
-  
-  user:any ={
-   name:'',
-    email: '',
-   password:'',
-   director:'',
-   addressLine:'',
-   city:'',
-   state:'',
-   country:'',
-   type:''
-  }
 
-  name:string;
-  password:string;
-  email:string;
-  director:string;
-  addressLine:string;
-  city:string;
-  state:string;
-  country:string;
-  type:string;
-  
+
+
   submit(ngodata:any){
-    // this.name= this.form.value['name'];
-    // this.email= this.form.value['email'];
-    // this.password= this.form.value['password'];
-    // this.director= this.form.value['director'];
-    // this.addressLine= this.form.value['adressLine'];
-    // this.city= this.form.value['city'];
-    // this.state= this.form.value['state'];
-    // this.country= this.form.value['country'];
-    // this.type= this.form.value['type'];
-    this.ng.create(ngodata).subscribe(response => alert(response));
+
+  
+        ngodata['stepStatus']='Registered';
+        ngodata['name']=localStorage.getItem('name');
+        ngodata['email']=localStorage.getItem('email');
+        ngodata['password']=localStorage.getItem('password');
+        this.ns.update(this.ngoid,ngodata).subscribe(response => alert(response));
           alert('registered Successfully !');
+          localStorage.setItem('stepStatus','Registered');
+          this.router.navigate(['/ngoDashboard/ngoregister']); 
 
   }
 
-  // myItem: any;
-  // storeName() {
-  //   localStorage.setItem('email', 'Angular');
-  //   localStorage.setItem('password', 'Angular');
-  //   // this.myItem = localStorage.getItem(this.key);
-  // }
-  // deleteName() {
-  //   localStorage.clear();
-  // }
+  
 }
 
 
